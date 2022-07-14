21639025815383ae54d3fbc7128d34394171cefe
g0khan0zkan <gokozkan89@gmail.com>
2022-07-14 20:42:06 +0300

    FastEndpoint first commit.

---
+++ MinimalApi/Program.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+using FastEndpoints.Swagger;
+using Pruducts.Api.Sevices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddFastEndpoints();
+builder.Services.AddSwaggerDoc();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+var app = builder.Build();
+app.UseFastEndpoints(x =>
+{
+  
+});
+
+app.UseHttpsRedirection();
+app.UseOpenApi();
+app.UseSwaggerUi3();
+app.Run();
+
